16d117b061b83cb6ed0cdb941e1c4c87edfe9e08
Fabio Maria Carlucci <fabiom.carlucci@yahoo.it>
2018-02-22 18:12:17 +0100

    data aug fix

--- data_loader.py
+++ data_loader.py
@@ -67,14 +67,12 @@ def get_office31_dataloader(case, batch_size):
         'webcam': [0.61197983011509638, 0.61876474000372972, 0.61729662103473015],
         'dslr': [],
         'imagenet': [0.485, 0.456, 0.406],
-        'special': [122.7, 116.7, 104.0]
     }
     stds = {
         'amazon': [0.27691643643313618, 0.28152348841965347, 0.28287296762830788],
         'webcam': [0.22763857108616978, 0.23339382150450594, 0.23722725519031848],
         'dslr': [],
         'imagenet': [0.229, 0.224, 0.225],
-        'special': [1.0, 1.0, 1.0]
     }
 
     img_size = (227, 227)
@@ -83,7 +81,7 @@ def get_office31_dataloader(case, batch_size):
         transforms.RandomCrop(img_size),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize(means['special'], stds['special']),
+        transforms.Normalize(means['imagenet'], stds['imagenet']),
     ]
 
     data_loader = data.DataLoader(
